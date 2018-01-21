ba621b43a1696fc411dc7777da7b3215ad88034d
ali <bdusaali@gmail.com>
2018-01-21 18:19:35 -0500

    new file added

---
+++ src/Rohan_1/Rabbi_1.java
@@ -0,0 +1,15 @@
+package Rohan_1;
+
+public class Rabbi_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("She is my waife");
+		System.out.println("She is very nice");
+		System.out.println("She is 35 years old");
+		System.out.println("She is always besy");
+
+
+	}
+
+}
